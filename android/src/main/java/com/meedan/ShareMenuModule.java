19354b73dc193157833518beabfc17cc5fd53d6f
Jared Roberts <orunin@gmail.com>
2016-08-25 19:07:27 -0400

    Prevent intents from triggering when launching an app from the Android's history

--- android/src/main/java/com/meedan/ShareMenuModule.java
+++ android/src/main/java/com/meedan/ShareMenuModule.java
@@ -10,16 +10,21 @@ import com.facebook.react.bridge.Callback;
 import com.meedan.ShareMenuPackage;
 
 import java.util.Map;
+import java.util.ArrayList;
+import java.lang.System;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 
 public class ShareMenuModule extends ReactContextBaseJavaModule {
   private Activity mActivity = null;
+  private ArrayList<Long> consumedIntents;
 
   public ShareMenuModule(ReactApplicationContext reactContext, Activity activity) {
     super(reactContext);
     mActivity = activity;
+    consumedIntents = new ArrayList<Long>();
   }
 
   @Override
@@ -33,6 +38,13 @@ public class ShareMenuModule extends ReactContextBaseJavaModule {
     String action = intent.getAction();
     String type = intent.getType();
     String inputText = intent.getStringExtra(Intent.EXTRA_TEXT);
-    successCallback.invoke(inputText);
+    Long currentIntent = intent.getLongExtra("id", -1);
+
+    if (!consumedIntents.contains(currentIntent)) {
+      Long id = System.currentTimeMillis();
+      intent.putExtra("id", id);
+      consumedIntents.add(id);
+      successCallback.invoke(inputText);
+    }
   }
 }
